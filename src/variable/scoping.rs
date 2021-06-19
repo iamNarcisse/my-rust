fc3d0cc095323235d2fe65d964ac8c740ce42396
iamNarcisse <mechikaegon@gmail.com>
2021-06-19 21:00:01 +0100

    Freezing variable

--- src/variable/scoping.rs
+++ src/variable/scoping.rs
@@ -1,3 +1,29 @@
+fn shadowing() {
+    let shadowed_binding = 1;
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+
+    println!("outside inner block:{}", shadowed_binding);
+
+    let shadowed_binding = 2;
+
+    println!("shadowed in outer block: {}", shadowed_binding);
+}
+
+fn freezing() {
+    let mut _to_be_freezed = 1;
+
+    {
+        let _to_be_freezed = _to_be_freezed;
+    }
+
+    _to_be_freezed = 10;
+}
 pub fn index() {
     let long_lived_binding = 1;
 
@@ -9,4 +35,8 @@ pub fn index() {
     // println!("outer short : {}", short_lived_binding);
 
     println!("outer long: {}", long_lived_binding);
+
+    shadowing();
+
+    freezing();
 }
