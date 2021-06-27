b3388724be509bd79587bc47676c4ac9c20d3a5d
iamNarcisse <mechikaegon@gmail.com>
2021-06-27 13:07:17 +0100

    Error handling with unwrap

--- src/error_handling/index.rs
+++ src/error_handling/index.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 fn drink(drink: &str) {
     if drink == "beer" {
         panic!("Aaaaaaaaaah!!!");
@@ -13,11 +15,24 @@ fn give_adult(drink: Option<&str>) {
         None => println!("Nothing provided"),
     }
 }
+
+fn drink_with_unwrap(drink: Option<&str>) {
+    let data = drink.unwrap();
+
+    if data == "lemonade" {
+        panic!("Oh no!, not lemonade again!")
+    }
+
+    println!("{}, How nice", data);
+}
+
 pub fn main() {
     // drink("beer");
     // drink("wine");
 
     let water = Some("Narcise");
-    let lemonade = Some("lemonade");
-    give_adult(water)
+    // let lemonade = Some("lemonade");
+    give_adult(water);
+    // give_adult(lemonade);
+    drink_with_unwrap(Some("lemonade"))
 }
