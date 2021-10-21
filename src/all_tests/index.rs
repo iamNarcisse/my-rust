c31ea44f6dce9ad797ffccf8d912aff2e29d97fc
Narcisse Egonu <mechikaegon@gmail.com>
2021-10-21 17:46:39 +0100

    Test sqrt

--- src/all_tests/index.rs
+++ src/all_tests/index.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 pub fn add_numbers(a:i32, b:i32) -> i32 {
    let result : i32 =  a + b;
 
@@ -10,6 +12,17 @@ pub fn add_numbers(a:i32, b:i32) -> i32 {
 pub fn bad_add_number(a : i32, b : i32)  -> i32{
     a - b
 }
+
+
+pub fn sqrt(number : f64) ->  Result<f64, String> {
+
+    if number >= 0.0{
+     return   Ok(number.powf(0.5))
+    }
+
+   Err("negative floats don't have square roots".to_owned())
+
+} 
 
 
 
@@ -30,5 +43,15 @@ mod tests {
        assert_eq!(bad_add_number(1, 2), 3);
     }
 
+
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
+    }
+    
+
+    
 
 }
